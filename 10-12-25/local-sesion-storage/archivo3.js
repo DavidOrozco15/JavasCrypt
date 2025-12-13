f03df4f2a5dd47fb07705215e5c63305fc338157
DavidOrozco <juanodavid94@gmail.com>
2025-12-12 20:39:59 -0500

    Se completo el ejercicio

---
+++ 10-12-25/local-sesion-storage/archivo3.js
@@ -0,0 +1,9 @@
+import * as archivo1 from "./archivo.js";
+import {PI as PIC} from "./archivo4.js";
+
+import mensaje from "./mensaje.js";
+
+console.log(archivo1.suma(25, 22));
+console.log(archivo1.PI);
+console.log(PIC);
+console.log(mensaje());
